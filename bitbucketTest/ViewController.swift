711abf79f25697855ff216a06a21532c88837ff6
William Musumba <w.e.musumba@gmail.com>
2017-07-31 18:08:18 -0500

    add need program for test

---
+++ bitbucketTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  bitbucketTest
+//
+//  Created by William Musumba on 31/07/2017.
+//  Copyright © 2017 William Musumba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("hello")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
